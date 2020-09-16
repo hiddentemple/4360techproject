8ca3ed7d530f79e72072accbd48945b66c3f5c94
Michael Newman <newms226@gmail.com>
2020-09-16 13:58:22 -0600

    CS4360-39 Foundation

--- cs4360/apps/crm/ui/src/app/app.module.ts
+++ cs4360/apps/crm/ui/src/app/app.module.ts
@@ -3,12 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { DevComponent } from './dev/dev.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {ContactBookModule} from "@crm/ng/contact-book";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DevComponent],
   imports: [
+    // Angular Modules
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    RouterModule,
+
+    // Core Modules
+
+    // Imported from Libs
+    ContactBookModule,
+
+    // Feature Modules
+
+    // Routing
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- cs4360/libs/crm/ng/contact-book/src/lib/contact-form/contact-form.component.ts
+++ cs4360/libs/crm/ng/contact-book/src/lib/contact-form/contact-form.component.ts
@@ -1,16 +1,11 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {ContactModel} from "@crm/shared/model";
+import {Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'cs4360-contact-form',
+  selector: 'crm-ui-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
-
-  @Input() contact: ContactModel
-
-  @Output() submit: EventEmitter<ContactModel>
 
   constructor() { }
 
