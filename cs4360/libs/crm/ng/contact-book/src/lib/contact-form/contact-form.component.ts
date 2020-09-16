b29cf18a158ec9854ed1cde75e1b4eeb53226084
Michael Newman <newms226@gmail.com>
2020-09-16 16:57:18 -0600

    CS4360-39 Dynamic Emails

--- cs4360/libs/crm/ng/contact-book/src/lib/contact-form/contact-form.component.ts
+++ cs4360/libs/crm/ng/contact-book/src/lib/contact-form/contact-form.component.ts
@@ -1,30 +1,48 @@
 import {Component, OnInit, Output, EventEmitter} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-export interface ContactFormEvent {
-  firstName : string;
-  lastName : string;
-}
+import {Form, FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {CreateContactRequest} from "../../../../../shared/src/lib/contracts/contact";
+
 @Component({
   selector: 'crm-ui-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
-  nameForm = new FormGroup({
-    firstName: new FormControl('', [Validators.required]),
-    lastName: new FormControl('', [Validators.required]),
+  contactForm: FormGroup;
 
-  })
-  @Output() submit = new EventEmitter <ContactFormEvent>();
-  constructor() { }
+  @Output() submit = new EventEmitter<CreateContactRequest>();
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      emails: this.fb.array([this.initEmail()])
+    });
   }
+
+  getEmailFormArray(): FormArray { return this.contactForm.controls.emails as FormArray; }
+
+  initEmail() {
+    return this.fb.group({
+      address: ['', [Validators.email, Validators.required]],
+      type: ['']
+    });
+  }
+
+  addEmail() {
+    const control: FormArray = this.getEmailFormArray();
+    control.push(this.initEmail());
+  }
+
+  removeEmail(i: number) {
+    const control: FormArray = this.getEmailFormArray();
+    control.removeAt(i);
+  }
+
   onSubmit(): void {
-    if (this.nameForm.status === "VALID"){
-      this.submit.emit(this.nameForm.value)
-    }else{
-      console.error("Try to submit when form is invalid.")
-    }
+    if (this.contactForm.valid) { this.submit.emit(this.contactForm.value); }
+    else { console.error("Try to submit when form is invalid."); }
   }
 }
