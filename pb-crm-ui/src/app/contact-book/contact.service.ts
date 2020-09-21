2c3c134785277f718476735b71201618f8d34426
Michael Newman <newms226@gmail.com>
2020-09-21 14:05:59 -0600

    update service

--- pb-crm-ui/src/app/contact-book/contact.service.ts
+++ pb-crm-ui/src/app/contact-book/contact.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {EMPTY, Observable, of} from "rxjs";
 import {ContactModel} from "../api/api-interfaces/contact/models/contact.model";
 import {CreateContactResponse} from "../api/api-interfaces/contact/contracts/create.contact";
@@ -10,48 +10,72 @@ import {DeleteContactResponse} from "../api/api-interfaces/contact/contracts/del
 })
 export class ContactService {
 
-  constructor() { }
+  constructor() {
+  }
 
   getContacts(): Observable<ContactModel[]> {
-    return of([{
-      id: 'contactid',
-      firstName: 'Ben',
-      lastName: 'Poe',
-      emails: [
-        {
-          id: '1',
-          address: 'benpoe@personal.com',
-          type: 'personal'
-        },
-        {
-          id: '2',
-          address: 'benpoe@work.com',
-          type: 'work'
-        },
-        {
-          id: '3',
-          address: 'benpoe@school.com',
-          type: 'school'
-        }
-      ],
-      phones: [
-        {
-          id: '1',
-          number: 3038675309,
-          type: 'personal home'
-        },
-        {
-          id: '2',
-          number: 3033334444,
-          type: 'personal cell'
-        },
-        {
-          id: '3',
-          number: 1012024040,
-          type: 'work'
-        }
-      ]
-    }])
+    return of([
+      {
+        id: 'contactid',
+        firstName: 'Ben',
+        lastName: 'Poe',
+        emails: [
+          {
+            id: '1',
+            address: 'benpoe@personal.com',
+            type: 'personal'
+          },
+          {
+            id: '2',
+            address: 'benpoe@work.com',
+            type: 'work'
+          },
+          {
+            id: '3',
+            address: 'benpoe@school.com',
+            type: 'school'
+          }
+        ],
+        phones: [
+          {
+            id: '1',
+            number: 3038675309,
+            type: 'personal home'
+          },
+          {
+            id: '2',
+            number: 3033334444,
+            type: 'personal cell'
+          },
+          {
+            id: '3',
+            number: 1012024040,
+            type: 'work'
+          }
+        ]
+      },
+      {
+        firstName: 'Michael',
+        lastName: 'Newman',
+        company: 'Charles Schwab',
+        emails: [
+          {
+            address: 'michael.newman@schwab.com',
+            type: 'work'
+          },
+          {
+            address: 'bluemaster92@gmail.com',
+            type: 'personal'
+          }
+        ],
+        phones: [
+          {
+            number: 4586985689,
+            type: 'Made up'
+          }
+        ]
+      }
+    ])
   }
 
   createContact(contact: ContactModel): Observable<CreateContactResponse> {
