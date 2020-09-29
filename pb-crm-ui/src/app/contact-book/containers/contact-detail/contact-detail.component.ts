7e05ab3b9b59b21756f2eeb2c4847e98032e256a
GitNickProgramming <n.programming0530@gmail.com>
2020-09-28 21:16:11 -0600

    cs4360-39 refactored Company in Contact Form

--- pb-crm-ui/src/app/contact-book/containers/contact-detail/contact-detail.component.ts
+++ pb-crm-ui/src/app/contact-book/containers/contact-detail/contact-detail.component.ts
@@ -1,20 +1,35 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {ContactModel} from "../../../api/api-interfaces/contact/models/contact.model";
+import {ContactModel} from '../../../api/api-interfaces/contact/models/contact.model';
 
 @Component({
   selector: 'app-contact-detail',
   template: `
-    <p>
-      contact-detail works!
-    </p>
+    <h1>{{title}}</h1>
+    <h2>Name: {{firstName}} {{lastName}}</h2>
+
   `,
   styleUrls: ['./contact-detail.component.scss']
 })
 export class ContactDetailComponent implements OnInit {
+  title: string;
+  firstName: string;
+  lastName: string;
 
-  @Input() contact: ContactModel
+  @Input() set contacts(contacts: ContactModel[]){
+    if (!contacts || contacts === []) {
+      console.log('Contacts received a falsey value: ', contacts);
+      return;
+    }
 
-  constructor() { }
+    console.log('Contact Table received new contacts: ', contacts);
+    this.dataSource.data = contacts;
+  }
+
+  constructor() {
+    this.title = 'Contact Details';
+    this.firstName = '';
+    this.lastName = '';
+  }
 
   ngOnInit(): void {
   }
