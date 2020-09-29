3cf8f373c6a38829c9d440c77f41de4b4f2f3e96
Michael Newman <newms226@gmail.com>
2020-09-29 14:44:25 -0600

    fix anoyance

--- pb-crm-ui/src/app/dev/dev.component.ts
+++ pb-crm-ui/src/app/dev/dev.component.ts
@@ -4,63 +4,11 @@ import {ContactModel} from '../api/api-interfaces/contact/models/contact.model';
 @Component({
   selector: 'app-dev',
   template: `
-    <div class="container">
-      <app-contact-detail [contact]="mockContact"></app-contact-detail>
-    </div>
-
   `
 })
 export class DevComponent implements OnInit {
-
-  mockContact: ContactModel = {
-    id: 'contactid',
-    firstName: 'Ben',
-    lastName: 'Poe',
-    emails: [
-      {
-        id: '1',
-        address: 'benpoe@personal.com',
-        type: 'personal'
-      },
-      {
-        id: '2',
-        address: 'benpoe@work.com',
-        type: 'work'
-      },
-      {
-        id: '3',
-        address: 'benpoe@school.com',
-        type: 'school'
-      }
-    ],
-    phones: [
-      {
-        id: '1',
-        number: 3038675309,
-        type: 'personal home'
-      },
-      {
-        id: '2',
-        number: 3033334444,
-        type: 'personal cell'
-      },
-      {
-        id: '3',
-        number: 1012024040,
-        type: 'work'
-      }
-    ]
-
-  };
-
-  constructor() {
-  }
-
   ngOnInit(): void {
   }
 
-  onSubmit(event: ContactModel) {
-    console.log('Submit', event);
-  }
 
 }
