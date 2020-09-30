31e2570700d2a5eb074bb1061385b8c793e6b71c
GitNickProgramming <n.programming0530@gmail.com>
2020-09-30 14:50:34 -0600

    cs4360-143 added phone/email/company to detail card

--- pb-crm-ui/src/app/contact-book/containers/contact-detail/contact-detail.component.ts
+++ pb-crm-ui/src/app/contact-book/containers/contact-detail/contact-detail.component.ts
@@ -5,7 +5,18 @@ import {ContactModel} from '../../../api/api-interfaces/contact/models/contact.m
   selector: 'app-contact-detail',
   template: `
     <div class="container-fluid">
-      <h2>Name: {{contact.firstName}} {{contact.lastName}}</h2>
+      <h2>{{contact.firstName}} {{contact.lastName}}</h2>
+      <h4>Company: {{contact.company}}</h4>
+      <div formArrayName="phones">Phone:
+        <div *ngFor="let phone of contact.phones; let i=index">
+          <ol>{{phone.type}} {{phone.number}}</ol>
+        </div>
+      </div>
+      <div formArrayName="emails">Email:
+        <div *ngFor="let email of contact.emails; let i=index">
+          <ol>{{email.type}} {{email.address}}</ol>
+        </div>
+      </div>
     </div>
 
   `,
