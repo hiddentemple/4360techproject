d033fd01f73e166c2094371ddbcfe61ce8f3c74b
GitNickProgramming <n.programming0530@gmail.com>
2020-10-01 14:06:19 -0600

    cs4360-143 mat table widths working

--- pb-crm-ui/src/app/contact-book/containers/contact-detail/contact-detail.component.ts
+++ pb-crm-ui/src/app/contact-book/containers/contact-detail/contact-detail.component.ts
@@ -7,7 +7,7 @@ import {ContactModel} from '../../../api/api-interfaces/contact/models/contact.m
     <div class="container-fluid">
       <h2>{{contact.firstName}} {{contact.lastName}}</h2>
       <h3>Company: {{contact.company}}</h3>
-      <app-phone-table [phones]="contact?.phones"></app-phone-table>
+      <app-phone-table [phones]="contact?.phones"></app-phone-table><br>
       <app-email-table [emails]="contact?.emails"></app-email-table>
     </div>
   `,
