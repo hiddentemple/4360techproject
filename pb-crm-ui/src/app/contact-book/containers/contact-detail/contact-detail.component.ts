ee6df53dcdb1f0349798efb7d5ba3e2d764c38f9
GitNickProgramming <n.programming0530@gmail.com>
2020-10-11 12:53:02 -0600

    CS4360-166 initial implementation of notes in contact-detail

--- pb-crm-ui/src/app/contact-book/containers/contact-detail/contact-detail.component.ts
+++ pb-crm-ui/src/app/contact-book/containers/contact-detail/contact-detail.component.ts
@@ -7,13 +7,22 @@ import {ContactModel} from '@hiddentemple/api-interfaces';
     <div class="container-fluid">
       <h2>{{contact.firstName}} {{contact.lastName}}</h2>
       <h3>Company: {{contact.company}}</h3>
+      <!-- Notes Input-->
+<!--      <div class="row" *ngIf="!show">-->
+        <mat-form-field class="col-12">
+          <mat-label>Notes</mat-label>
+          <textarea matInput maxlength="250" placeholder="Ex. This is a note..." formControlName="notes"></textarea>
+        </mat-form-field>
+<!--      </div>-->
       <app-phone-table [phones]="contact?.phones"></app-phone-table><br>
-      <app-email-table [emails]="contact?.emails"></app-email-table>
+      <app-email-table [emails]="contact?.emails"></app-email-table><br>
+<!--      <button mat-raised-button (click)="show =! show" type="button" color="accent">Add Notes</button>-->
     </div>
   `,
   styleUrls: ['./contact-detail.component.scss']
 })
 export class ContactDetailComponent {
+  show = true;
 
   @Input() contact: ContactModel;
 
