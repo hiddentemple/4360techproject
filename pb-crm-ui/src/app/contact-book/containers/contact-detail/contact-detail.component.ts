f1a064d5aad446849c7e0c1050599c7786983fab
GitNickProgramming <n.programming0530@gmail.com>
2020-10-14 15:40:45 -0600

    CS4360-166 changed button ui to be clearer

--- pb-crm-ui/src/app/contact-book/containers/contact-detail/contact-detail.component.ts
+++ pb-crm-ui/src/app/contact-book/containers/contact-detail/contact-detail.component.ts
@@ -7,11 +7,11 @@ import {ContactModel} from '@hiddentemple/api-interfaces';
     <div class="container-fluid">
       <h2>{{contact.firstName}} {{contact.lastName}}</h2>
       <h3>Company: {{contact.company}}</h3>
-      <app-phone-table [phones]="contact.phones"></app-phone-table><br>
+      <app-phone-table *ngIf="[] ==! _contact?.phones" [phones]="contact?.phones"></app-phone-table><br>
       <app-email-table [emails]="contact?.emails"></app-email-table><br>
       <mat-form-field class="col-12">
         <mat-label>Notes</mat-label>
-        <textarea [value]="contact?.notes" matInput readonly maxlength="250" placeholder="Ex. This is a note..." ></textarea>
+        <textarea [value]="contact?.notes" matInput readonly ></textarea>
       </mat-form-field>
     </div>
   `,
@@ -19,7 +19,7 @@ import {ContactModel} from '@hiddentemple/api-interfaces';
 })
 export class ContactDetailComponent {
 
-  private _contact: ContactModel;
+  _contact: ContactModel;
 
   @Input() set contact(contact: ContactModel) {
     if (!contact) {
