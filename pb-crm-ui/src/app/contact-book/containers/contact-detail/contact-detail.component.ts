6582c044133fab3eb01cfffb881430b0dc71df2e
GitNickProgramming <n.programming0530@gmail.com>
2020-10-17 15:25:31 -0600

    CS4360-166 refactored code

--- pb-crm-ui/src/app/contact-book/containers/contact-detail/contact-detail.component.ts
+++ pb-crm-ui/src/app/contact-book/containers/contact-detail/contact-detail.component.ts
@@ -9,14 +9,12 @@ import {ContactModel} from '@hiddentemple/api-interfaces';
       <h3>Company: {{contact.company}}</h3>
       <app-phone-table *ngIf="this._contact?.phones.length > 0" [phones]="contact?.phones"></app-phone-table><br>
       <app-email-table *ngIf="this._contact?.emails.length > 0" [emails]="contact?.emails"></app-email-table><br>
-      <div *ngIf="this._contact?.notes == null; else notes_NotEmpty"></div>
-      <ng-template #notes_NotEmpty>
+      <div *ngIf="this._contact?.notes != null">
         <mat-form-field class="col-12">
           <mat-label>Notes</mat-label>
           <textarea matInput [value]="contact?.notes" readonly></textarea>
         </mat-form-field>
-      </ng-template>
-
+      </div>
     </div>
   `,
   styleUrls: ['./contact-detail.component.scss']
