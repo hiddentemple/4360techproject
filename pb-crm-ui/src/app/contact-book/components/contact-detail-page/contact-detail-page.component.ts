f81e40815588fad1ad264a04e561ab9434a9ad6b
GitNickProgramming <n.programming0530@gmail.com>
2020-10-22 16:42:08 -0600

    CS4360-196 ...mostly working...

--- pb-crm-ui/src/app/contact-book/components/contact-detail-page/contact-detail-page.component.ts
+++ pb-crm-ui/src/app/contact-book/components/contact-detail-page/contact-detail-page.component.ts
@@ -1,18 +1,22 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import {ContactModel} from '@hiddentemple/api-interfaces';
 import {ContactCacheService} from '../../services/contact-cache.service';
 
 @Component({
   selector: 'app-contact-detail-page',
   template: `
-    <p>Contact Detail Page Works</p>
-    <app-contact-detail [contact]="contact"></app-contact-detail>
+    <div class="container">
+        <app-contact-detail [contact]="contact"></app-contact-detail>
+    </div>
   `,
   styles: []
 })
 export class ContactDetailPageComponent implements OnInit {
   contact: ContactModel;
+
+  @Output() delete = new EventEmitter<ContactModel>();
+  @Output() edit = new EventEmitter<ContactModel>();
 
   constructor(
     private route: ActivatedRoute,
@@ -23,5 +27,8 @@ export class ContactDetailPageComponent implements OnInit {
     const id: string = this.route.snapshot.paramMap.get('id');
     this.cache.getContact(id).subscribe(contact => this.contact = contact);
   }
+
+  onEdit(contact: ContactModel) { this.edit.emit(contact); }
+  onDelete(contact: ContactModel) { this.delete.emit(contact); }
 
 }
--- pb-crm-ui/src/app/contact-book/contact-book-routing.module.ts
+++ pb-crm-ui/src/app/contact-book/contact-book-routing.module.ts
@@ -1,13 +1,16 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ContactBookHomeComponent} from './components/contact-book-home/contact-book-home.component';
+import {ContactDetailPageComponent} from './components/contact-detail-page/contact-detail-page.component';
 
 export const ContactBookRoutes = {
-  home: 'contacts'
+  home: 'contacts',
+  id: 'contacts'
 };
 
 const routes: Routes = [
-  { path: ContactBookRoutes.home, component: ContactBookHomeComponent }
+  { path: ContactBookRoutes.home, component: ContactBookHomeComponent },
+  { path: 'contacts/:id', component: ContactDetailPageComponent}
 ];
 
 @NgModule({
