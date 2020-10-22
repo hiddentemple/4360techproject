4c2c62a020a29e46bdae07cf4eacf937b554ab16
Michael Newman <newms226@gmail.com>
2020-10-22 15:22:42 -0600

    CS4360-203 Correct PR

--- pb-crm-ui/src/app/contact-book/containers/contact-detail/contact-detail.component.ts
+++ pb-crm-ui/src/app/contact-book/containers/contact-detail/contact-detail.component.ts
@@ -11,22 +11,31 @@ import {Portal, TemplatePortal} from "@angular/cdk/portal";
   selector: 'app-contact-detail',
   template: `
     <div class="container-fluid">
-      <h2>{{contact.firstName}} {{contact.lastName}}</h2>
+      <div class="row">
+        <h2>{{contact.firstName}} {{contact.lastName}}</h2>
+
+        <span class="add-spacer"></span>
+
+        <button mat-icon-button
+                matTooltip="Edit"
+                matTooltipPosition=""
+                (click)="onEdit(contact)">
+          <mat-icon>edit</mat-icon>
+        </button>
+        <button mat-icon-button
+                matTooltip="Delete"
+                matTooltipPosition="left"
+                (click)="onDelete(contact)">
+          <mat-icon>delete</mat-icon>
+        </button>
+      </div>
+
+
+
       <h3 *ngIf="_contact.company">Company: {{contact.company}}</h3>
       <app-phone-table *ngIf="this._contact?.phones.length > 0" [phones]="contact?.phones"></app-phone-table><br>
       <app-email-table *ngIf="this._contact?.emails.length > 0" [emails]="contact?.emails"></app-email-table><br>
-      <button mat-icon-button
-              matTooltip="Edit"
-              matTooltipPosition=""
-              (click)="onEdit(contact)">
-        <mat-icon>edit</mat-icon>
-      </button>
-      <button mat-icon-button
-              matTooltip="Delete"
-              matTooltipPosition="left"
-              (click)="onDelete(contact)">
-        <mat-icon>delete</mat-icon>
-      </button>
+
       <div *ngIf="this._contact?.notes != null">
         <mat-form-field class="col-12">
           <mat-label>Notes</mat-label>
@@ -34,45 +43,19 @@ import {Portal, TemplatePortal} from "@angular/cdk/portal";
         </mat-form-field>
       </div>
     </div>
-    <ng-template #contactDetail>
-      <app-contact-detail [contact]="selectedContact"
-                          (delete)="deleteContact($event)"
-                          (edit)="setEditContact($event)" >
-      </app-contact-detail>
-    </ng-template>
+
   `,
-  styleUrls: ['./contact-detail.component.scss']
+  styles: [
+    `.add-spacer {
+      flex: 1 1 auto;
+    }`
+  ]
 })
 export class ContactDetailComponent {
-  contacts: ContactModel[];
-  selectedContact: ContactModel;
-  showTable = true;
-  showDetail = false;
-  tableSize: TableSize = TableSize.FULL;
-
-  selectedPortal: Portal<any>;
-  detailPortal: TemplatePortal<any>;
-  createPortal: TemplatePortal<any>;
-  editPortal: TemplatePortal<any>;
-
-  @ViewChild('contactDetail') contactDetail: TemplateRef<unknown>;
-  @ViewChild('createContactForm') createContactForm: TemplateRef<unknown>;
-  @ViewChild('editContactForm') editContactForm: TemplateRef<unknown>;
+  _contact: ContactModel;
 
   @Output() delete = new EventEmitter<ContactModel>();
   @Output() edit = new EventEmitter<ContactModel>();
-  @Output() view = new EventEmitter<ContactModel>();
-
-  @Input() size: TableSize;
-
-  constructor(
-    private dialog: MatDialog,
-    private contactCache: ContactCacheService,
-    private snackbar: MatSnackBar,
-    //private viewContainerRef: ViewContainerRef
-  ) {
-  }
-  _contact: ContactModel;
 
   @Input() set contact(contact: ContactModel) {
     if (!contact) {
@@ -90,41 +73,4 @@ export class ContactDetailComponent {
 
   onEdit(contact: ContactModel) { this.edit.emit(contact); }
   onDelete(contact: ContactModel) { this.delete.emit(contact); }
-
-  setEditContact(contact: ContactModel) {
-    this.selectedContact = contact;
-    this.selectedPortal = this.editPortal;
-    //this.openRightPanel();
-  }
-
-  setViewContact(contact: ContactModel) {
-    this.selectedContact = contact;
-    this.selectedPortal = this.detailPortal;
-    //this.openRightPanel();
-  }
-
-  async editContact(contact: ContactModel) {
-    await this.contactCache.updateContact(contact).then(updatedContact =>
-      this.setViewContact(updatedContact)
-    );
-  }
-  deleteContact(contact: ContactModel) {
-    const dialogRef = this.dialog.open(DeleteConfirmationComponent);
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.contactCache.deleteContact(contact).subscribe(() => {
-          this.snackbar.open('Contact Deleted', 'Close', {duration: 1000});
-          if (this.selectedContact === contact) {
-            this.reset();
-          }
-        });
-      }
-    });
-  }
-  private reset() {
-    this.showDetail = false;
-    this.tableSize = TableSize.FULL;
-    this.selectedContact = undefined;
-    this.selectedPortal = undefined;
-  }
 }
--- pb-crm-ui/src/app/contact-book/containers/contact-table/contact-table.component.ts
+++ pb-crm-ui/src/app/contact-book/containers/contact-table/contact-table.component.ts
@@ -38,13 +38,11 @@ export class ContactTableComponent implements AfterViewInit {
   @Input() size: TableSize;
   @Input() set contacts(contacts: ContactModel[]) { this.setContacts(contacts); }
 
-  get displayedColumns(): string[] { return this.getColumns();
-  }
+  get displayedColumns(): string[] { return this.getColumns(); }
 
   constructor() {
     this.dataSource = new MatTableDataSource([]);
   }
-
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
