5f422ce804d4e917e97d6a3abcaf5acf7406cd89
GitNickProgramming <n.programming0530@gmail.com>
2020-10-22 16:13:06 -0600

    CS4360-196 implementing id route

--- pb-crm-ui/src/app/contact-book/components/contact-detail-page/contact-detail-page.component.ts
+++ pb-crm-ui/src/app/contact-book/components/contact-detail-page/contact-detail-page.component.ts
@@ -1,11 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ContactModel} from '@hiddentemple/api-interfaces';
+import {ContactCacheService} from '../../services/contact-cache.service';
 
 @Component({
   selector: 'app-contact-detail-page',
   template: `
     <p>Contact Detail Page Works</p>
+    <app-contact-detail [contact]="contact"></app-contact-detail>
   `,
   styles: []
 })
@@ -13,12 +15,13 @@ export class ContactDetailPageComponent implements OnInit {
   contact: ContactModel;
 
   constructor(
-    private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private cache: ContactCacheService
   ) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(map => this.contact);
+    const id: string = this.route.snapshot.paramMap.get('id');
+    this.cache.getContact(id).subscribe(contact => this.contact = contact);
   }
 
 }
