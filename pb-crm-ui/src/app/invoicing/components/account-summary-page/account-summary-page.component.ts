bb484e7232e0cda253e240767b63dc23cf4c4dc6
Michael Newman <newms226@gmail.com>
2020-11-20 17:38:00 -0700

    Routing Updates and Navigation

--- pb-crm-ui/src/app/invoicing/components/account-summary-page/account-summary-page.component.ts
+++ pb-crm-ui/src/app/invoicing/components/account-summary-page/account-summary-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {InvoicingRoutes} from "../../invoicing-routing.module";
 
 @Component({
   selector: 'app-account-summary-page',
@@ -6,15 +8,29 @@ import { Component, OnInit } from '@angular/core';
     <p>
       account-summary-page works!
     </p>
+    <button (click)="goToDashboard()">Go To Account Dashboard</button>
+    <button (click)="goToInvoice()">Go To Invoice</button>
   `,
   styles: [
   ]
 })
 export class AccountSummaryPageComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+    const id = route.snapshot.paramMap.get('id'); // get the id from the URL
+    // Get the account from cache
+  }
 
   ngOnInit(): void {
   }
 
+  goToDashboard() {
+    this.router.navigate([InvoicingRoutes.home])
+  }
+
+  goToInvoice() {
+    const id = "asgasdf2" // id of invoice
+    this.router.navigate([InvoicingRoutes.invoicesWithoutID, id])
+  }
 }
