84cadd024b8176afaf558759d7829172caa46b76
GitNickProgramming <n.programming0530@gmail.com>
2020-11-23 12:15:02 -0700

    CS4360-361 initial account-cache

---
+++ pb-crm-ui/src/app/invoicing/services/account-cache.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AccountCacheService } from './account-cache.service';
+
+describe('AccountCacheService', () => {
+  let service: AccountCacheService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AccountCacheService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
