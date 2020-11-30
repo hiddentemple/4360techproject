e45fa9a0a90859f3f4355cb3997b3892dc1230a6
GitNickProgramming <n.programming0530@gmail.com>
2020-11-30 11:59:45 -0700

    CS4360-361 corrected routes to reflect "accounts"

--- pb-crm-ui/src/app/invoicing/services/AccountService.ts
+++ pb-crm-ui/src/app/invoicing/services/AccountService.ts
@@ -18,21 +18,21 @@ export class AccountService {
   constructor(private apiService: ApiService) {}
 
   getAccounts(): Observable<GetAllAccountResponse>  {
-    return this.apiService.get<GetAllAccountResponse>('/api/contact', {});
+    return this.apiService.get<GetAllAccountResponse>('/api/account', {});
   }
 
   getAccount(id: string): Observable<GetAccountResponse>{
-    return this.apiService.get<GetAccountResponse>('/api/contact/' + id);
+    return this.apiService.get<GetAccountResponse>('/api/account/' + id);
   }
 
   async createAccount(account: AccountModel): Promise<CreateAccountResponse> {
     const req: CreateAccountRequest = { account };
-    return this.apiService.post<CreateAccountResponse>('/api/contact', req, {}).toPromise();
+    return this.apiService.post<CreateAccountResponse>('/api/account', req, {}).toPromise();
   }
 
   async updateAccount(account: AccountModel): Promise<UpdateAccountResponse>{
     const req: UpdateAccountRequest = { account }; // cast will fail if invalid model passed
-    return this.apiService.put<UpdateAccountResponse>('/api/contact/' + account.id, req, {}).toPromise();
+    return this.apiService.put<UpdateAccountResponse>('/api/account/' + account.id, req, {}).toPromise();
   }
 
   deleteAccount(id: string): Observable<any> {
