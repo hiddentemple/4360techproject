e7706d482601dab93464c4969c5a0ea56ca32551
Vinson <55514029+vinsonchin@users.noreply.github.com>
2020-12-04 10:42:50 -0700

    line item table

--- pb-crm-ui/src/app/invoicing/invoicing.module.ts
+++ pb-crm-ui/src/app/invoicing/invoicing.module.ts
@@ -6,13 +6,16 @@ import { AccountDashboardPageComponent } from './components/account-dashboard-pa
 import { AccountListComponent } from './containers/account-list/account-list.component';
 import { AccountSummaryPageComponent } from './components/account-summary-page/account-summary-page.component';
 import { InvoicePageComponent } from './components/invoice-page/invoice-page.component';
+import { LineItemTableComponent } from './containers/line-item-table/line-item-table/line-item-table.component';
+import {MatTableModule} from "@angular/material/table";
 
 
 @NgModule({
-  declarations: [AccountDashboardPageComponent, AccountListComponent, AccountSummaryPageComponent, InvoicePageComponent],
-  imports: [
-    CommonModule,
-    InvoicingRoutingModule
-  ]
+  declarations: [AccountDashboardPageComponent, AccountListComponent, AccountSummaryPageComponent, InvoicePageComponent, LineItemTableComponent],
+    imports: [
+        CommonModule,
+        InvoicingRoutingModule,
+        MatTableModule
+    ]
 })
 export class InvoicingModule { }
